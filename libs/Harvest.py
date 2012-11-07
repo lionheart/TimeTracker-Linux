81444c54bac5ee1396d20823b1b0627293b31c6c
Alex Goretoy <alex@goretoy.com>
2012-11-07 02:58:45 -0800

    new api client everything working perfectly
    Signed-off-by: Alex Goretoy <alex@goretoy.com>

--- libs/Harvest.py
+++ libs/Harvest.py
@@ -1,13 +1,26 @@
+'''
+>>> import Harvest
+>>> harvest = Harvest.Harvest("https://COMPANYNAME.harvestapp.com", "EMAIL", "PASSWORD")
+>>> harvest.status()[""]
+>>> data = {"notes":"test note", "project_id":"PROJECT_ID","hours":"1.0", "task_id": "TASK_ID"}
+>>> harvest.add(data)
+>>> data['notes'] = "another test"
+>>> harvest.update("ENTRY_ID", data)
+>>> harvest.get_today()
+
+'''
 import urllib2
 from urllib import urlencode
 from base64 import b64encode
-from dateutil.parser import parse as parseDate
 
 import json
-from StringIO import StringIO
+from StringIO import StringIO #to parse json
+
+from xml.dom.minidom import Document #to create xml out of dict
 
 class HarvestError(Exception):
     pass
+
 
 class Harvest(object):
     def __init__(self, uri, email, password):
@@ -23,16 +36,16 @@ class Harvest(object):
         return self._request("GET", 'http://harveststatus.com/status.json')
 
     def get_today(self):
-        return self._request('GET', "%s/daily"%self.uri)
+        return self._request('GET', "%s/daily" % self.uri)
 
     def get_day(self, day_of_the_year=1, year=2012):
         return self._request('GET', '%s/daily/%s/%s' % (self.uri, day_of_the_year, year))
 
     def get_entry(self, entry_id):
-        return self._request("GET", "%s/daily/show/%s"%(self.uri, entry_id))
+        return self._request("GET", "%s/daily/show/%s" % (self.uri, entry_id))
 
     def toggle_timer(self, entry_id):
-        return self._request("GET", "%s/daily/timer/%s" %(self.uri, entry_id))
+        return self._request("GET", "%s/daily/timer/%s" % (self.uri, entry_id))
 
     def add(self, data):
         return self._request("POST", '%s/daily/add' % self.uri, data)
@@ -41,9 +54,9 @@ class Harvest(object):
         return self._request("DELETE", "%s/daily/delete/%s" % (self.uri, entry_id))
 
     def update(self, entry_id, data):
-        return self._request('POST', '%s/daily/update/%s'% (self.uri, entry_id), urlencode(data))
+        return self._request('POST', '%s/daily/update/%s' % (self.uri, entry_id), data)
 
-    def _request(self, type = "GET", url = "", data = None ):
+    def _request(self, type="GET", url="", data=None ):
         """
         type = request type, eg. GET, POST, DELETE
         url = full url to make request to, eg. https://MYCOMPANY.harvestapp.com/daily
@@ -52,16 +65,22 @@ class Harvest(object):
         """
         if type != "DELETE":
             if data:
+                self.headers["Accept"] = 'application/xml'
+                data = self._build_xml(data)
                 request = urllib2.Request(url=url, data=data, headers=self.headers)
-                print data
+                self.headers["Accept"] = 'application/json'
             else:
-                request = urllib2.Request(url=url, headers=self.headers)
+                if not url.endswith(".json"): #dont put headers it a status request
+                    request = urllib2.Request(url=url, headers=self.headers)
+                else:
+                    request = urllib2.Request(url=url)
 
             try:
                 r = urllib2.urlopen(request)
-                j = r.read()
-                j = StringIO(j)
-                return json.load(j)
+                if not data:
+                    j = r.read()
+                    j = StringIO(j)
+                    return json.load(j)
             except Exception as e:
                 raise HarvestError(e)
         else:
@@ -69,6 +88,23 @@ class Harvest(object):
                 opener = urllib2.build_opener(urllib2.HTTPHandler)
                 request = urllib2.Request(url=url, headers=self.headers)
                 request.get_method = lambda: "DELETE"
-                return opener.open(request)
+                return opener.open(request).read()
             except Exception as e:
                 raise HarvestError(e)
+
+    def _build_xml(self, data_dict):
+        def _builder(root, data):
+            if type(data) == dict:
+                for d in data.keys():
+                    tag = doc.createElement(d)
+                    root.appendChild(tag)
+                    _builder(tag, data[d])
+            else:
+                root.appendChild(doc.createTextNode(str(data)))
+
+        doc = Document()
+        root = doc.createElement("request")
+        doc.appendChild(root)
+        _builder(root, data_dict)
+        xml = doc.toprettyxml()
+        return xml
